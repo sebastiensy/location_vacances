735427b160fa6a5cd342b923e414e765a4b4b628
Julien WAZAN <julien.wazan@gmail.com>
2015-09-26 10:25:31 +0200

    commentaire

--- location-vac/src/main/java/fr/uha/miage/locvac/LocationVacApplication.java
+++ location-vac/src/main/java/fr/uha/miage/locvac/LocationVacApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LocationVacApplication {
 
-	// test
+	// pas de test
+	//blabla
     public static void main(String[] args) {
         SpringApplication.run(LocationVacApplication.class, args);
     }
