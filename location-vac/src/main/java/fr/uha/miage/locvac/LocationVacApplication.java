be8a8ba183db2bff1fa997610d7794c6ad9f7f6b
sebastiensy <seb6890@gmail.com>
2015-09-26 09:27:32 +0200

    Premier commit

---
+++ location-vac/src/main/java/fr/uha/miage/locvac/LocationVacApplication.java
@@ -0,0 +1,12 @@
+package fr.uha.miage.locvac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationVacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationVacApplication.class, args);
+    }
+}
