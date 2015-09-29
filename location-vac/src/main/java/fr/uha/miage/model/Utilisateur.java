0bfd4748658991f3e04df9b644d3c2f615889746
sebastiensy <seb6890@gmail.com>
2015-09-29 10:42:41 +0200

    Modification model utilisateur

--- location-vac/src/main/java/fr/uha/miage/model/Utilisateur.java
+++ location-vac/src/main/java/fr/uha/miage/model/Utilisateur.java
@@ -1,9 +1,85 @@
 package fr.uha.miage.model;
 
-import org.springframework.boot.orm.jpa.EntityScan;
+import java.sql.Date;
 
-@EntityScan
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Utilisateur {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String nom;
+	private String prenom;
+	private String email;
+	private String mdp;
+	private Date dateNaissance;
+	private String tel;
+	private boolean droit;
+	private String photo;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMdp() {
+		return mdp;
+	}
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public boolean isDroit() {
+		return droit;
+	}
+	public void setDroit(boolean droit) {
+		this.droit = droit;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	
+	
 	
 
 }
