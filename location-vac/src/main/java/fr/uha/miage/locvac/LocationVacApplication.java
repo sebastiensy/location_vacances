91da4fd50ae1ee9acd0594ee7efcf4a21309aea8
sebastiensy <seb6890@gmail.com>
2015-09-29 09:40:52 +0200

    Suppression commentaires

--- location-vac/src/main/java/fr/uha/miage/locvac/LocationVacApplication.java
+++ location-vac/src/main/java/fr/uha/miage/locvac/LocationVacApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LocationVacApplication {
 
-	// pas de test
-	//blabla
     public static void main(String[] args) {
         SpringApplication.run(LocationVacApplication.class, args);
     }
